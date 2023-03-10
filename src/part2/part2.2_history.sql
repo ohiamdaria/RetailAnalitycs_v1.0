d36bd3d424b641b79b77636ae6a723acb8b611b4
Vovolandinio <nanowatcher65@gmail.com>
2023-03-10 20:01:52 +0300

    added triggers for MATERIALIZED view history

--- src/part2/part2.2_history.sql
+++ src/part2/part2.2_history.sql
@@ -1,4 +1,4 @@
-CREATE VIEW history AS
+CREATE MATERIALIZED VIEW history AS
 WITH helpers
          AS
          (SELECT public.cards.customer_id,
@@ -18,8 +18,47 @@ SELECT customer_id                  AS "Customer_ID",
        transaction_id               AS "Transaction_ID",
        transaction_datetime         AS "Transaction_DateTime",
        group_id                     AS "Group_ID",
-       ROUND(SUM(Group_Cost), 2)      AS "Group_Cost",
+       ROUND(SUM(Group_Cost), 2)    AS "Group_Cost",
        ROUND(SUM(sku_summ), 2)      AS "Group_Summ",
        ROUND(SUM(sku_summ_paid), 2) AS "Group_Summ_Paid"
 FROM helpers
 GROUP BY customer_id, transaction_id, transaction_datetime, group_id;
+
+CREATE OR REPLACE FUNCTION update_history() RETURNS trigger AS
+$$
+BEGIN
+    REFRESH MATERIALIZED VIEW history;
+    RETURN NULL;
+END;
+$$
+    LANGUAGE plpgsql;
+
+CREATE TRIGGER update_history_trigger_transactions
+    AFTER INSERT OR UPDATE OR DELETE
+    ON public.transactions
+    FOR EACH ROW
+EXECUTE FUNCTION update_history();
+
+CREATE TRIGGER update_history_trigger_sku
+    AFTER INSERT OR UPDATE OR DELETE
+    ON public.sku
+    FOR EACH ROW
+EXECUTE FUNCTION update_history();
+
+CREATE TRIGGER update_history_trigger_cards
+    AFTER INSERT OR UPDATE OR DELETE
+    ON public.cards
+    FOR EACH ROW
+EXECUTE FUNCTION update_history();
+
+CREATE TRIGGER update_history_trigger_checks
+    AFTER INSERT OR UPDATE OR DELETE
+    ON public.checks
+    FOR EACH ROW
+EXECUTE FUNCTION update_history();
+
+CREATE TRIGGER update_history_trigger_stores
+    AFTER INSERT OR UPDATE OR DELETE
+    ON public.stores
+    FOR EACH ROW
+EXECUTE FUNCTION update_history();
