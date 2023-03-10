5545b1bc83f3e056758b85358a89d0e01e65aafd
Vovolandinio <nanowatcher65@gmail.com>
2023-03-10 19:55:37 +0300

    added period view

--- src/part2/part2.2_history.sql
+++ src/part2/part2.2_history.sql
@@ -18,7 +18,8 @@ SELECT customer_id                  AS "Customer_ID",
        transaction_id               AS "Transaction_ID",
        transaction_datetime         AS "Transaction_DateTime",
        group_id                     AS "Group_ID",
-       ROUND(Group_Cost, 2)      AS "Group_Cost",
-       ROUND(sku_summ, 2)      AS "Group_Summ",
-       ROUND(sku_summ_paid, 2) AS "Group_Summ_Paid"
-FROM helpers;
+       ROUND(SUM(Group_Cost), 2)      AS "Group_Cost",
+       ROUND(SUM(sku_summ), 2)      AS "Group_Summ",
+       ROUND(SUM(sku_summ_paid), 2) AS "Group_Summ_Paid"
+FROM helpers
+GROUP BY customer_id, transaction_id, transaction_datetime, group_id;
--- src/part2/part2.3_periods.sql
+++ src/part2/part2.3_periods.sql
@@ -1,23 +1,58 @@
+CREATE MATERIALIZED VIEW periods AS
 WITH helpers AS
-    (
-        SELECT
-            cards.customer_id,
-            sku.group_id,
-            transactions.transaction_datetime,
-            checks.sku_discount,
-            checks.sku_summ,
-            public.stores.sku_purchase_price * checks.sku_amount AS Group_Cost
-        FROM transactions
-        INNER JOIN cards on cards.customer_card_id = transactions.customer_card_id
-        INNER JOIN sku on sku.sku_id = transactions.transaction_id
-        INNER JOIN checks on sku.sku_id = checks.sku_id
-            INNER JOIN stores on sku.sku_id = stores.sku_id AND stores.transaction_store_id = transactions.transaction_store_id
-    )
-SELECT
-    helpers.customer_id AS "Customer_ID",
-    helpers.group_id AS "Group_ID",
-    MIN(helpers.transaction_datetime) AS "First_Group_Purchase_Date",
-    MAX(helpers.transaction_datetime) AS "Last_Group_Purchase_Date",
-    COUNT(group_id)  AS "Group_Purchase"
+         (SELECT public.transactions.transaction_datetime,
+                 public.cards.customer_id,
+                 public.sku.group_id,
+                 public.checks.sku_summ,
+                 public.checks.sku_discount
+          FROM public.transactions
+                   JOIN cards
+                        ON cards.customer_card_id = transactions.customer_card_id
+                   JOIN checks
+                        ON checks.transaction_id = transactions.transaction_id
+                   JOIN sku
+                        ON sku.sku_id = checks.sku_id)
+SELECT customer_id                                                                          AS "Customer_ID",
+       group_id                                                                             AS "Group_ID",
+       MIN(transaction_datetime)                                                            AS "First_Group_Purchase_Date",
+       MAX(transaction_datetime)                                                            AS "Last_Group_Purchase_Date",
+       COUNT(*)                                                                             AS "Group_Purchase",
+       ((MAX(transaction_datetime)::date - MIN(transaction_datetime)::date) + 1) / COUNT(*) AS "Group_Frequency",
+       ROUND(COALESCE(MIN(CASE
+                              WHEN sku_discount = 0 THEN NULL
+                              ELSE sku_discount / sku_summ END), 0), 2)                     AS "Group_Min_Discount"
 FROM helpers
 GROUP BY customer_id, group_id;
+
+CREATE OR REPLACE FUNCTION update_periods() RETURNS trigger AS
+$$
+BEGIN
+    REFRESH MATERIALIZED VIEW periods;
+    RETURN NULL;
+END;
+$$
+    LANGUAGE plpgsql;
+
+CREATE TRIGGER update_periods_trigger_transactions
+    AFTER INSERT OR UPDATE OR DELETE
+    ON public.transactions
+    FOR EACH ROW
+EXECUTE FUNCTION update_periods();
+
+CREATE TRIGGER update_periods_trigger_sku
+    AFTER INSERT OR UPDATE OR DELETE
+    ON public.sku
+    FOR EACH ROW
+EXECUTE FUNCTION update_periods();
+
+CREATE TRIGGER update_periods_trigger_cards
+    AFTER INSERT OR UPDATE OR DELETE
+    ON public.cards
+    FOR EACH ROW
+EXECUTE FUNCTION update_periods();
+
+CREATE TRIGGER update_periods_trigger_checks
+    AFTER INSERT OR UPDATE OR DELETE
+    ON public.checks
+    FOR EACH ROW
+EXECUTE FUNCTION update_periods();
