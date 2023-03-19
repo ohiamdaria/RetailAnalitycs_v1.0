25fdcce80a20ddf4d63966621ef5843c364fab49
Vovolandinio <nanowatcher65@gmail.com>
2023-03-19 13:47:36 +0300

    changed the views, I make a selection from the new view

--- src/part2/part2.2_history.sql
+++ src/part2/part2.2_history.sql
@@ -1,19 +1,4 @@
-CREATE MATERIALIZED VIEW history AS
-WITH helpers
-         AS
-         (SELECT public.cards.customer_id,
-                 public.transactions.transaction_id,
-                 public.transactions.transaction_datetime,
-                 public.sku.group_id,
-                 public.checks.sku_summ,
-                 public.checks.sku_summ_paid,
-                 public.stores.sku_purchase_price * checks.sku_amount AS Group_Cost
-          FROM public.transactions
-                   INNER JOIN cards on cards.customer_card_id = transactions.customer_card_id
-                   INNER JOIN checks on transactions.transaction_id = checks.transaction_id
-                   INNER JOIN sku on sku.sku_id = checks.sku_id
-                   INNER JOIN stores on sku.sku_id = stores.sku_id AND
-                                        stores.transaction_store_id = transactions.transaction_store_id)
+CREATE VIEW history AS
 SELECT customer_id                  AS "Customer_ID",
        transaction_id               AS "Transaction_ID",
        transaction_datetime         AS "Transaction_DateTime",
@@ -21,44 +6,8 @@ SELECT customer_id                  AS "Customer_ID",
        ROUND(SUM(Group_Cost), 2)    AS "Group_Cost",
        ROUND(SUM(sku_summ), 2)      AS "Group_Summ",
        ROUND(SUM(sku_summ_paid), 2) AS "Group_Summ_Paid"
-FROM helpers
-GROUP BY customer_id, transaction_id, transaction_datetime, group_id;
-
-CREATE OR REPLACE FUNCTION update_history() RETURNS trigger AS
-$$
-BEGIN
-    REFRESH MATERIALIZED VIEW history;
-    RETURN NULL;
-END;
-$$
-    LANGUAGE plpgsql;
-
-CREATE TRIGGER update_history_trigger_transactions
-    AFTER INSERT OR UPDATE OR DELETE
-    ON public.transactions
-    FOR EACH ROW
-EXECUTE FUNCTION update_history();
-
-CREATE TRIGGER update_history_trigger_sku
-    AFTER INSERT OR UPDATE OR DELETE
-    ON public.sku
-    FOR EACH ROW
-EXECUTE FUNCTION update_history();
-
-CREATE TRIGGER update_history_trigger_cards
-    AFTER INSERT OR UPDATE OR DELETE
-    ON public.cards
-    FOR EACH ROW
-EXECUTE FUNCTION update_history();
-
-CREATE TRIGGER update_history_trigger_checks
-    AFTER INSERT OR UPDATE OR DELETE
-    ON public.checks
-    FOR EACH ROW
-EXECUTE FUNCTION update_history();
-
-CREATE TRIGGER update_history_trigger_stores
-    AFTER INSERT OR UPDATE OR DELETE
-    ON public.stores
-    FOR EACH ROW
-EXECUTE FUNCTION update_history();
+FROM suppoort_view
+GROUP BY customer_id,
+         transaction_id,
+         transaction_datetime,
+         group_id;
--- src/part2/part2.3_periods.sql
+++ src/part2/part2.3_periods.sql
@@ -1,17 +1,4 @@
-CREATE MATERIALIZED VIEW periods AS
-WITH helpers AS
-         (SELECT public.transactions.transaction_datetime,
-                 public.cards.customer_id,
-                 public.sku.group_id,
-                 public.checks.sku_summ,
-                 public.checks.sku_discount
-          FROM public.transactions
-                   JOIN cards
-                        ON cards.customer_card_id = transactions.customer_card_id
-                   JOIN checks
-                        ON checks.transaction_id = transactions.transaction_id
-                   JOIN sku
-                        ON sku.sku_id = checks.sku_id)
+CREATE VIEW periods AS
 SELECT customer_id                                                                          AS "Customer_ID",
        group_id                                                                             AS "Group_ID",
        MIN(transaction_datetime)                                                            AS "First_Group_Purchase_Date",
@@ -21,38 +8,5 @@ SELECT customer_id                                                              
        ROUND(COALESCE(MIN(CASE
                               WHEN sku_discount = 0 THEN NULL
                               ELSE sku_discount / sku_summ END), 0), 2)                     AS "Group_Min_Discount"
-FROM helpers
+FROM suppoort_view
 GROUP BY customer_id, group_id;
-
-CREATE OR REPLACE FUNCTION update_periods() RETURNS trigger AS
-$$
-BEGIN
-    REFRESH MATERIALIZED VIEW periods;
-    RETURN NULL;
-END;
-$$
-    LANGUAGE plpgsql;
-
-CREATE TRIGGER update_periods_trigger_transactions
-    AFTER INSERT OR UPDATE OR DELETE
-    ON public.transactions
-    FOR EACH ROW
-EXECUTE FUNCTION update_periods();
-
-CREATE TRIGGER update_periods_trigger_sku
-    AFTER INSERT OR UPDATE OR DELETE
-    ON public.sku
-    FOR EACH ROW
-EXECUTE FUNCTION update_periods();
-
-CREATE TRIGGER update_periods_trigger_cards
-    AFTER INSERT OR UPDATE OR DELETE
-    ON public.cards
-    FOR EACH ROW
-EXECUTE FUNCTION update_periods();
-
-CREATE TRIGGER update_periods_trigger_checks
-    AFTER INSERT OR UPDATE OR DELETE
-    ON public.checks
-    FOR EACH ROW
-EXECUTE FUNCTION update_periods();
