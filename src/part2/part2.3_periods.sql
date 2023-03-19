4de8d60f36347993e56e1881a395e8339a4b271f
Vovolandinio <nanowatcher65@gmail.com>
2023-03-19 16:24:03 +0300

    convert to numeric expression

--- src/part2/part2.3_periods.sql
+++ src/part2/part2.3_periods.sql
@@ -4,7 +4,7 @@ SELECT customer_id                                                              
        MIN(transaction_datetime)                                                            AS "First_Group_Purchase_Date",
        MAX(transaction_datetime)                                                            AS "Last_Group_Purchase_Date",
        COUNT(transaction_id)                                                                             AS "Group_Purchase",
-       (MAX(transaction_datetime)::date - MIN(transaction_datetime)::date + 1) / COUNT(transaction_id)         AS "Group_Frequency",
+       ROUND((MAX(transaction_datetime)::date - MIN(transaction_datetime)::date + 1)::numeric / COUNT(transaction_id), 2)         AS "Group_Frequency",
        ROUND(COALESCE(MIN(CASE
                               WHEN sku_discount = 0 THEN NULL
                               ELSE sku_discount / sku_summ END), 0), 2)                     AS "Group_Min_Discount"
