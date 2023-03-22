faf5d86321ecea37701a121e99681622a133d1f3
Harmonica Lucius <harmonic@student.21-school.ru>
2023-03-22 21:08:41 +0300

    add public to the tables name

--- src/part2/part2_customers.sql
+++ src/part2/part2_customers.sql
@@ -19,8 +19,8 @@ CREATE OR REPLACE FUNCTION end_date_for_analysis() RETURNS timestamp
 AS
 $$
 BEGIN
-    RETURN (SELECT MAX(analysis_formation)
-            FROM date_of_analysis_formation);
+    RETURN (SELECT MAX(public.date_of_analysis_formation.analysis_formation)
+            FROM public.date_of_analysis_formation);
 END;
 $$ LANGUAGE plpgsql;
 
@@ -37,17 +37,20 @@ AS
 $$
 DECLARE
 BEGIN
-    RETURN QUERY (SELECT support_view.customer_id,
-                         COALESCE(ROUND(SUM(transaction_summ) / NULLIF(COUNT(support_view.customer_id), 0)),
-                                  0)                 AS average_check,
-                         COALESCE(ROUND(extract(EPOCH FROM (MAX(transaction_datetime) - MIN(transaction_datetime)))::float
-                             / 86400.0 / NULLIF(COUNT(transaction_id), 0)), 0)
-                                                     AS frequency,
-                         ROUND(extract(EPOCH FROM (end_date_for_analysis() - MAX(transaction_datetime)))::float /
-                               86400.0)              AS inactive_period,
-                         COUNT(transaction_store_id) AS sum_transaction_store
-                  FROM support_view
-                  GROUP BY support_view.customer_id);
+    RETURN QUERY (SELECT public.support_view.customer_id,
+                         COALESCE(ROUND(SUM(public.support_view.transaction_summ) /
+                                        NULLIF(COUNT(public.support_view.customer_id), 0)),
+                                  0)                                     AS average_check,
+                         COALESCE(ROUND(extract(EPOCH FROM (MAX(public.support_view.transaction_datetime) -
+                                                            MIN(public.support_view.transaction_datetime)))::float
+                             / 86400.0 / NULLIF(COUNT(public.support_view.transaction_id), 0)), 0)
+                                                                         AS frequency,
+                         ROUND(extract(EPOCH FROM (end_date_for_analysis() -
+                                                   MAX(public.support_view.transaction_datetime)))::float /
+                               86400.0)                                  AS inactive_period,
+                         COUNT(public.support_view.transaction_store_id) AS sum_transaction_store
+                  FROM public.support_view
+                  GROUP BY public.support_view.customer_id);
 END;
 $$ LANGUAGE plpgsql;
 
@@ -61,17 +64,18 @@ AS
 $$
 DECLARE
 BEGIN
-    RETURN QUERY (WITH uniq_customer_transaction_table AS (SELECT base.customer_id,
-                                                                  base.transaction_store_id,
-                                                                  COUNT(base.transaction_id) AS uniq_transaction
-                                                           FROM support_view base
-                                                           GROUP BY base.customer_id, base.transaction_store_id),
-                       rank_store_table AS (SELECT base.customer_id,
-                                                   base.transaction_datetime,
-                                                   base.transaction_store_id,
+    RETURN QUERY (WITH uniq_customer_transaction_table AS (SELECT public.support_view.customer_id,
+                                                                  public.support_view.transaction_store_id,
+                                                                  COUNT(public.support_view.transaction_id) AS uniq_transaction
+                                                           FROM public.support_view
+                                                           GROUP BY public.support_view.customer_id,
+                                                                    public.support_view.transaction_store_id),
+                       rank_store_table AS (SELECT public.support_view.customer_id,
+                                                   public.support_view.transaction_datetime,
+                                                   public.support_view.transaction_store_id,
                                                    RANK()
-                                                   OVER (PARTITION BY base.customer_id ORDER BY base.transaction_datetime DESC) AS rank
-                                            FROM support_view base),
+                                                   OVER (PARTITION BY public.support_view.customer_id ORDER BY public.support_view.transaction_datetime DESC) AS rank
+                                            FROM public.support_view),
                        last_three_store_table AS (SELECT rank_store_table.customer_id,
                                                          rank_store_table.transaction_store_id,
                                                          COUNT(rank_store_table.transaction_store_id) AS count
@@ -86,23 +90,27 @@ BEGIN
                                                                NULLIF(info.sum_transaction_store, 0),
                                                                0)) AS part
                                             FROM uniq_customer_transaction_table
-                                                     LEFT JOIN support_view base ON base.transaction_store_id =
-                                                                                        uniq_customer_transaction_table.transaction_store_id
+                                                     LEFT JOIN public.support_view
+                                                               ON public.support_view.transaction_store_id =
+                                                                  uniq_customer_transaction_table.transaction_store_id
                                                      LEFT JOIN get_base_customer_info() info
                                                                ON uniq_customer_transaction_table.customer_id = info.customer_id
-                                            ORDER BY base.transaction_datetime DESC),
-                       max_part_store_table AS (SELECT ps.customer_id,
-                                                       ps.transaction_store_id,
-                                                       MAX(part) AS max
-                                                FROM part_store_table ps
-                                                GROUP BY ps.customer_id, ps.transaction_store_id),
-                       range_part_store_table AS (SELECT st.customer_id,
-                                                         st.transaction_store_id,
-                                                         RANK() OVER (PARTITION BY st.customer_id ORDER BY st.max DESC) AS runk,
-                                                         (SELECT MAX(base.transaction_datetime)
-                                                          FROM support_view base
-                                                          WHERE base.transaction_store_id = st.transaction_store_id)    AS max_date
-                                                  FROM max_part_store_table st),
+                                            ORDER BY public.support_view.transaction_datetime DESC),
+                       max_part_store_table AS (SELECT part_store_table.customer_id,
+                                                       part_store_table.transaction_store_id,
+                                                       MAX(part_store_table.part) AS max
+                                                FROM part_store_table
+                                                GROUP BY part_store_table.customer_id,
+                                                         part_store_table.transaction_store_id),
+                       range_part_store_table AS (SELECT max_part_store_table.customer_id,
+                                                         max_part_store_table.transaction_store_id,
+                                                         RANK()
+                                                         OVER (PARTITION BY max_part_store_table.customer_id ORDER BY max_part_store_table.max DESC) AS runk,
+                                                         (SELECT MAX(public.support_view.transaction_datetime)
+                                                          FROM public.support_view
+                                                          WHERE public.support_view.transaction_store_id =
+                                                                max_part_store_table.transaction_store_id)                                           AS max_date
+                                                  FROM max_part_store_table),
                        primary_store
                            AS (SELECT DISTINCT ON (last_three_store_table.customer_id) last_three_store_table.customer_id,
                                                                                        CASE
@@ -140,10 +148,10 @@ CREATE MATERIALIZED VIEW customers
              "Customer_Primary_Store")
 AS
 WITH churn_rate_table AS (SELECT customer_id,
-                                 ROUND(COALESCE(inactive_period /
-                                                NULLIF(frequency, 0), 0)) AS churn_rate
-                          FROM get_base_customer_info()),
-     all_segments_table AS (SELECT ci.customer_id,
+                                 ROUND(COALESCE(info.inactive_period /
+                                                NULLIF(info.frequency, 0), 0)) AS churn_rate
+                          FROM get_base_customer_info() AS info),
+     all_segments_table AS (SELECT customer_info.customer_id,
                                    churn_rate_table.churn_rate,
                                    CASE
                                        WHEN (percent_rank() OVER (ORDER BY average_check DESC) <= 0.1) THEN 'High'
@@ -154,21 +162,26 @@ WITH churn_rate_table AS (SELECT customer_id,
                                        WHEN (percent_rank() OVER (ORDER BY frequency DESC) <= 0.35) THEN 'Occasionally'
                                        ELSE 'Rarely' END AS frequency_segment,
                                    CASE
-                                       WHEN churn_rate < 2 THEN 'Low'
-                                       WHEN churn_rate >= 2 AND churn_rate < 5 THEN 'Medium'
+                                       WHEN churn_rate_table.churn_rate < 2 THEN 'Low'
+                                       WHEN churn_rate_table.churn_rate >= 2 AND churn_rate_table.churn_rate < 5
+                                           THEN 'Medium'
                                        ELSE 'High' END   AS churn_segment
-                            FROM get_base_customer_info() ci
-                                     LEFT JOIN churn_rate_table ON ci.customer_id = churn_rate_table.customer_id
-                            ORDER BY average_check DESC),
+                            FROM get_base_customer_info() customer_info
+                                     LEFT JOIN churn_rate_table
+                                               ON customer_info.customer_id = churn_rate_table.customer_id
+                            ORDER BY customer_info.average_check DESC),
      number_segment_table AS (SELECT all_segments_table.customer_id,
-                                     segment.segment,
+                                     public.segment.segment,
                                      all_segments_table.average_segment,
                                      all_segments_table.frequency_segment,
                                      all_segments_table.churn_segment
                               FROM all_segments_table
-                                       LEFT JOIN segment ON (segment.average_check = all_segments_table.average_segment
-                                  AND segment.customer_frequency = all_segments_table.frequency_segment
-                                  AND segment.customer_churn = all_segments_table.churn_segment))
+                                       LEFT JOIN public.segment
+                                                 ON (public.segment.average_check = all_segments_table.average_segment
+                                                     AND public.segment.customer_frequency =
+                                                         all_segments_table.frequency_segment
+                                                     AND
+                                                     public.segment.customer_churn = all_segments_table.churn_segment))
 
 SELECT base_info.customer_id,
        base_info.average_check,
