2ab76448cc5877e4ed45ddd94718d8d7ab09a097
Gloria Gideon <gloriagi@at-p9.msk.21-school.ru>
2023-05-09 20:21:36 +0300

    ok

--- src/part4/part4.sql
+++ src/part4/part4.sql
@@ -11,46 +11,34 @@
 -- максимальный индекс оттока
 -- максимальная доля транзакций со скидкой (в процентах)
 -- допустимая доля маржи (в процентах)
---
---
--- Определение условия предложения
---
---
--- Выбор метода расчета среднего чека. Существует возможность
--- выбора метода расчета среднего чека – за определенный период времени
--- или за определенное количество последних транзакций. Метод расчета
--- вручную определяется пользователем.
---
---
--- Пользователь выбирает методику расчета по периоду, после чего
--- указывает первую и последнюю даты периода, за который
--- необходимо рассчитать средний чек для всей совокупности
--- клиентов, попавших в выборку. При этом последняя дата
--- указываемого периода должна быть позже первой, а указанный
--- период должен быть внутри общего анализируемого периода. В
--- случае указания слишком ранней или слишком поздней даты
--- система автоматически подставляет дату, соответственно, начала
--- или окончания анализируемого периода. Для расчета учитываются
--- все транзакции, совершенные каждым конкретным клиентом в
--- течение заданного периода.
---
---
--- Пользователь выбирает методику расчета по количеству последних
--- транзакций, после чего вручную указывает количество
--- транзакций, по которым необходимо рассчитать средний чек. Для
--- расчета среднего чека берется заданное пользователем
--- количество транзакций, начиная с самой последней в обратном
--- хронологическом порядке. В случае, если каким-либо клиентом из
--- выборки за весь анализируемый период совершено меньше
--- указанного количества транзакций, для анализа используется
--- имеющееся количество транзакций.
 -- CREATE FUNCTION average_check(method integer, first_date date, last_date date, count_of_transactions integer,
 --                               coefficient_of_increase_check float, max_churn_rate float,
 --                               max_share_of_transactions_with_discount float, acceptable_margin_share float)
+
+-- CREATE MATERIALIZED VIEW information AS (
+-- SELECT sp.customer_id,
+--                                       sp.group_id,
+--                                       sp.transaction_id,
+--                                       sp.sku_summ_paid - sp.sku_discount AS sku_total_price,
+--                                       sp.transaction_datetime,
+--                                       gr.group_affinity_index::numeric,
+--                                       gr.group_discount_share::numeric,
+--                                       gr.group_churn_rate::numeric,
+--                                       gr.group_margin::numeric
+--                                FROM public.support_view AS sp
+--                                INNER JOIN public.groups AS gr ON (sp.customer_id = gr.customer_id AND sp.group_id = gr.group_id)
+-- );
+
+
+
 SELECT * FROM date_of_analysis_formation;
-DROP FUNCTION average_check(method integer, first_date date, last_date date, coefficient_of_increase_check float);
-SELECT public.average_check(1, '2018-08-21', '2021-08-21', 2.345);
-CREATE FUNCTION average_check(method integer, first_date date, last_date date, coefficient_of_increase_check numeric, max_churn_rate float)
+DROP FUNCTION average_check(method integer, first_date date, last_date date, coefficient_of_increase_check numeric, max_churn_rate float);
+DROP FUNCTION average_check(method integer, first_date date, last_date date, coefficient_of_increase_check numeric, max_churn_rate numeric,
+                               max_share_of_transactions_with_discount numeric, acceptable_margin_share numeric);
+SELECT public.average_check(1, '2018-08-21', '2021-08-21', 2.345, 12, 2, 10);
+-- CREATE FUNCTION average_check(method integer, first_date date, last_date date, coefficient_of_increase_check numeric, max_churn_rate float)
+CREATE FUNCTION average_check(method integer, first_date date, last_date date, coefficient_of_increase_check numeric, max_churn_rate numeric,
+                               max_share_of_transactions_with_discount numeric, acceptable_margin_share numeric)
 RETURNS TABLE
             (
 --                 Customer_ID BIGINT,
@@ -60,7 +48,12 @@ RETURNS TABLE
 
                     customer_id BIGINT,
                     group_id BIGINT,
-                    Average_Check numeric
+--                     transcationd_id BIGINT,
+--                     acceptable_margin_share2 numeric,
+                    group_affinity_index numeric
+--                     group_discount_share numeric,
+--                     group_churn_rate numeric,
+--                     group_margin numeric
             )
 AS
 $$
@@ -70,7 +63,9 @@ BEGIN
 --    IF first_date < public.date_of_analysis_formation.analysis_formation THEN first_date = public.date_of_analysis_formation.analysis_formation;
 --     IF last_date > CURRENT_DATE THEN last_date = current_date;
     RETURN QUERY (
-        WITH Method_of_Count AS (SELECT public.support_view.customer_id,
+        -- для первого пункта - поставить триггер на изменения переменной, чтобы изменялось представление группы? как сделать с миним затратами по времени
+
+            WITH Method_of_Count AS (SELECT public.support_view.customer_id,
                                       public.support_view.group_id,
                                       public.support_view.transaction_id,
                                       public.support_view.sku_summ_paid - public.support_view.sku_discount AS sku_total_price
@@ -82,18 +77,27 @@ BEGIN
                                      SUM(Method_of_Count.sku_total_price) / COUNT(Method_of_Count.transaction_id) * coefficient_of_increase_check AS Average_Check
                                FROM Method_of_Count
                                GROUP BY Method_of_Count.customer_id, Method_of_Count.group_id),
-            sort_churn_rate AS (SELECT target_value_of_average_check.customer_id,
-                                       target_value_of_average_check.group_id
-                                FROM target_value_of_average_check INNER JOIN public.groups ON
-                                      (target_value_of_average_check.customer_id = public.groups.customer_id AND target_value_of_average_check.group_id = public.groups.group_id)
-                                WHERE public.groups.group_churn_rate < ),
-            max_affinity_index AS (SELECT target_value_of_average_check.customer_id,
-                                         -- target_value_of_average_check.group_id,
-                                         MAX(public.groups.Group_Affinity_Index)
-                                   FROM target_value_of_average_check INNER JOIN public.groups ON
-                                      (target_value_of_average_check.customer_id = public.groups.customer_id AND target_value_of_average_check.group_id = public.groups.group_id)
-                                  GROUP BY target_value_of_average_check.customer_id)
-            SELECT * FROM max_affinity_index);
+--             -- target_value_of_average_check - 3 пункт
+--
+            group_for AS (SELECT  gr.customer_id,
+                                          gr.group_id,
+                                          gr.group_margin * 0.2 ::numeric,
+                                          MAX(gr.group_affinity_index)::numeric AS group_affinity_index
+                                  FROM groups AS gr
+--                                   RIGHT JOIN target_value_of_average_check ON (gr.customer_id = target_value_of_average_check.customer_id AND
+--                                                                                gr.group_id = target_value_of_average_check.group_id)
+                                  WHERE gr.group_churn_rate <= 10 AND gr.group_discount_share < 10
+                                  GROUP BY gr.customer_id, gr.group_id, gr.group_margin)
+--             average_margin_group AS (SELECT group_for.customer_id,
+--                                             group_for.group_id,
+--                                             public.groups.group_margin * acceptable_margin_share::numeric
+--                                      FROM group_for INNER JOIN public.groups ON (public.groups.customer_id = group_for.customer_id AND
+--                                                                                public.groups.group_id = group_for.group_id))
+
+            -- group_for - 4 пункт
+            -- актуальная маржа по группе и средняя маржа - это одно и то же?
+            SELECT * FROM group_for);
 END;
 $$
 LANGUAGE plpgsql;
+
